cf5f40f337b8f92208c21bbc7440b97af86f3b3f
gagaruk <red8gams@gmail.com>
2026-07-25 13:58:09 +0200

    stepperDriver interface

--- cubus_hardware/include/config.h
+++ cubus_hardware/include/config.h
@@ -1,1 +1,46 @@
 #pragma once
+#include <Arduino.h>
+
+typedef struct
+{
+    uint8_t in1;
+    uint8_t in2;
+    uint8_t in3;
+    uint8_t in4;
+} stepperPins_t;
+
+namespace Stepper
+{
+    namespace pins
+    {
+        constexpr stepperPins_t m1{0, 0, 0, 0};
+        constexpr stepperPins_t m2{0, 0, 0, 0};
+        constexpr stepperPins_t m3{0, 0, 0, 0};
+        constexpr stepperPins_t m4{0, 0, 0, 0};
+        constexpr stepperPins_t m5{0, 0, 0, 0};
+        constexpr stepperPins_t m6{0, 0, 0, 0};
+    }
+    namespace turnConstants
+    {
+        constexpr static double turnMotorSpeed = 10;
+        constexpr static double shiftMotorSpeed = 10;
+        constexpr static uint8_t microstep_table[16][4] = {
+            {255, 0, 0, 0},
+            {236, 98, 0, 0},
+            {180, 180, 0, 0},
+            {98, 236, 0, 0},
+            {0, 255, 0, 0},
+            {0, 236, 98, 0},
+            {0, 180, 180, 0},
+            {0, 98, 236, 0},
+            {0, 0, 255, 0},
+            {0, 0, 236, 98},
+            {0, 0, 180, 180},
+            {0, 0, 98, 236},
+            {0, 0, 0, 255},
+            {98, 0, 0, 236},
+            {180, 0, 0, 180},
+            {236, 0, 0, 98}};
+
+    }
+}
--- cubus_hardware/lib/StepperDriver/StepperDriver.h
+++ cubus_hardware/lib/StepperDriver/StepperDriver.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <Arduino.h>
+#include "AccelStepper.h"
+
+class c_stepper
+{
+private:
+    int current_angle;
+    int targer_angle;
+
+    void _set_trapezoidal();
+    void _microstep();
+
+    uint8_t _in1;
+    uint8_t _in2;
+    uint8_t _in3;
+    uint8_t _in4;
+
+    int _speed;
+    bool _hold;
+
+public:
+    c_stepper(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4);
+    void move_to(int angle);
+    void set_speed(int speed);
+    void hold();
+    void release();
+};
